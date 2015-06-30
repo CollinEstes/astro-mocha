42f1c517818c386ba7315b52f247ba10056b283d
Collin Estes <collin.j.estes@nasa.gov>
2015-06-30 12:55:11 -0500

    fixed path issue

--- index.js
+++ index.js
@@ -12,13 +12,14 @@
 'use strict';
 var buildArgs = require('./src/buildArgs')
 	, path = require('path')
+	, fs = require('fs')
 	;
 
 
 module.exports = function (projectDir, options) {
 
 	var args = buildArgs(projectDir, options)
-		, cmd = path.resolve('./node_modules/.bin/mocha');
+		, cmd = path.resolve(fs.realpathSync(__dirname), './node_modules/.bin/mocha');
 
 	// set up
 	return {cmd: cmd, args: args};
